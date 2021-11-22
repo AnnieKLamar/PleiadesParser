a8c83d29f90ddc92631a140cdfb7e1ebe80b9320
AnnieKLamar <38513786+AnnieKLamar@users.noreply.github.com>
2021-11-21 21:37:54 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'PleiadesParser',
   packages = ['PleiadesParser'],
-  version = '1.0',
+  version = '1.1',
   license='MIT',
   description = 'Downloads most recent data from Pleiades and parses data in to Python objects.',
   author = 'Annie K. Lamar',
   author_email = 'kalamar@stanford.edu',
   url = 'https://github.com/AnnieKLamar/PleiadesParser',
-  download_url = 'https://github.com/AnnieKLamar/PleiadesParser/archive/refs/tags/v2.0.tar.gz',
+  download_url = 'https://github.com/AnnieKLamar/PleiadesParser/archive/refs/tags/v1.1.tar.gz',
   keywords = ['Pleiades', 'JSON', 'Stoa'],
   install_requires=[ 
           'wget',
@@ -22,9 +22,6 @@ setup(
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3'
   ],
 )
